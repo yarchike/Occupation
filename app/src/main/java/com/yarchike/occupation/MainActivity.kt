c85eee8276370833bfc829b35d9a69a4a5523e39
Yarchik <yarchike@gmail.com>
2020-09-23 23:11:47 +0300

    Добавил запись и чтение игнора Добавил ожидание в 4 секунды

--- app/src/main/java/com/yarchike/occupation/MainActivity.kt
+++ app/src/main/java/com/yarchike/occupation/MainActivity.kt
@@ -1,38 +1,93 @@
 package com.yarchike.occupation
 
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val list = readBase()
-        var listIgnore = ArrayList<Int>()
+        var listIgnore = readIgnore()
+        val h = Handler(){
+            buttonIdea.setVisibility(View.VISIBLE)
+            true
+        }
 
         buttonIdea.setOnClickListener() {
-            var randomInteger = (1..list.size - 1).shuffled().first().toInt()
-            for(i in list) {
-                if (randomInteger in listIgnore) {
-                    randomInteger = (1..list.size - 1).shuffled().first().toInt()
-                } else {
-                    listIgnore.add(randomInteger)
-                    textIdea.text = list[randomInteger]
-                    break
+            if (list.size <= listIgnore.size) {
+                Toast.makeText(this@MainActivity, "Идеи закончились", Toast.LENGTH_LONG).show()
+            } else {
+                var randomInteger = (0..list.size - 1).shuffled().first().toInt()
+                for (i in list) {
+                    if (randomInteger in listIgnore) {
+                        randomInteger = (0..list.size - 1).shuffled().first().toInt()
+                    } else {
+                        listIgnore.add(randomInteger)
+                        try {
+                            val file = File(cacheDir, FILE_NAME)
+                            val writer = BufferedWriter(FileWriter(file))
+                            var str = ""
+                            for (i in listIgnore) {
+                                str += i.toString() + ";"
+                            }
+                            str = str.substring(0, str.length - 1)
+                            writer.write(str)
+                            writer.close()
+                        } catch (e: Exception) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Ошибка записи в файл",
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                        }
+                        textIdea.text = list[randomInteger]
+                        break
+                    }
                 }
-                Log.d("my", randomInteger.toString())
             }
+            buttonIdea.setClickable(false)
+            buttonIdea.setVisibility(View.INVISIBLE)
+            //buttonIdea.setVisibility(View.VISIBLE)
+            Thread {
+                try {
+                    Thread.sleep(3000)
+                    buttonIdea.setClickable(true)
+                    h.sendEmptyMessage(0)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }.start()
+
         }
-        buttonTest.setOnClickListener(){
-            val text = textIdea.text.toString()
-            val fos = openFileOutput(FILE_NAME, MODE_PRIVATE)
-            fos.write(text.toByteArray())
-        }
+
+
     }
 
+    fun readIgnore(): ArrayList<Int> {
+        var arraylist = ArrayList<Int>()
+        try {
+            val file = File(cacheDir, FILE_NAME)
+            val reader = BufferedReader(FileReader(file))
+            val line = reader.readLine()
+            val array = line.split(";")
+            for (i in array) {
+                arraylist.add(i.toInt())
+            }
+        } catch (e: Exception) {
 
+        }
+        return arraylist
+
+
+    }
 
     fun readBase(): List<String> {
         val str = getString(R.string.base_idea)
